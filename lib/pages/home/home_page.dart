c9f7c80ad47635b461d8ab5180be67eb61dc37db
Jhonatan Cortez <jhonatancortez708@gmail.com>
2021-07-11 20:40:00 -0600

    Layout home agregando fondos y clippath

--- lib/pages/home/home_page.dart
+++ lib/pages/home/home_page.dart
@@ -1,45 +1,67 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_custom_clippers/flutter_custom_clippers.dart';
 
 class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.black,
       body: SafeArea(
-        child: Column(
-          children: [
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: [
-                Image.asset(
-                  'assets/img/logo_app.png',
-                  width: 150,
-                  height: 100,
+        child: Container(
+          decoration: BoxDecoration(
+            gradient: LinearGradient(
+              begin: Alignment.topRight,
+              end: Alignment.bottomLeft,
+              colors: [
+                Colors.black,
+                Colors.grey.shade800
+              ]
+            )
+          ),
+
+          child: Column(
+            children: [
+              ClipPath(
+                clipper: DiagonalPathClipperTwo(),
+                child: Container(
+                  color: Colors.white,
+                  height: MediaQuery.of(context).size.height * 0.30,
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      Image.asset(
+                        'assets/img/logo_app.png',
+                        width: 150,
+                        height: 100,
+                      ),
+                      Text('Facil y rapido')
+                    ],
+                  ),
                 ),
-                Text('Facil y rapido')
-              ],
-            ),
+              ),
 
-            SizedBox(height: 50),
-            Text('Selecciona tu rol'),
-            SizedBox(height: 50),
+              SizedBox(height: 50),
+              Text('Selecciona tu rol'),
+              SizedBox(height: 50),
 
-            CircleAvatar(
-              backgroundImage: AssetImage('assets/img/pasajero.png'),
-              radius: 50,
-              backgroundColor: Colors.black,
-            ),
-            SizedBox(height: 10),
-            Text('Cliente'),
+              CircleAvatar(
+                backgroundImage: AssetImage('assets/img/pasajero.png'),
+                radius: 50,
+                backgroundColor: Colors.grey[800],
+              ),
+              SizedBox(height: 10),
+              Text('Cliente', style: TextStyle(color: Colors.white)),
 
-            SizedBox(height: 50),
-            CircleAvatar(
-              backgroundImage: AssetImage('assets/img/driver.png'),
-              radius: 50,
-              backgroundColor: Colors.black,
-            ),
-            SizedBox(height: 10),
-            Text('Conductor'),
-          ],
+              SizedBox(height: 50),
+              CircleAvatar(
+                backgroundImage: AssetImage('assets/img/driver.png'),
+                radius: 50,
+                backgroundColor: Colors.grey[800],
+              ),
+              SizedBox(height: 10),
+              Text('Conductor', style: TextStyle(color: Colors.white)),
+            ],
+          ),
         ),
       ),
     );
