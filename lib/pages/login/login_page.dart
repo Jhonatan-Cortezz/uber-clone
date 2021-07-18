8c9d44eabf62dfb8e2305e6b554bab45cd80c483
Jhonatan-Cortez <jhonatancortez708@gmail.com>
2021-07-17 22:43:46 -0600

    Login page parte 2

--- lib/pages/login/login_page.dart
+++ lib/pages/login/login_page.dart
@@ -13,7 +13,16 @@ class _LoginPageState extends State<LoginPage> {
     return Scaffold(
         appBar: AppBar(),
         body: Column(
-          children: [_bannerClipPath()],
+          children: [
+            _bannerClipPath(),
+            _textDescription(),
+            _textLogin(),
+            Expanded(child: Container()),
+            _textFieldEmail(),
+            _textFieldPassword(),
+            _buttonLogin(),
+            _dontHaveAccount()
+          ],
         ));
   }
 
@@ -42,6 +51,85 @@ class _LoginPageState extends State<LoginPage> {
             )
           ],
         ),
+      ),
+    );
+  }
+
+  Widget _textDescription() {
+    return Container(
+      alignment: Alignment.centerLeft,
+      margin: EdgeInsets.symmetric(horizontal: 30, vertical: 10),
+      child: Text(
+        'Continua con tu',
+        style: TextStyle(
+            color: Colors.black54, fontSize: 24, fontFamily: 'NimbusSans'),
+      ),
+    );
+  }
+
+  Widget _textLogin() {
+    return Container(
+      alignment: Alignment.centerLeft,
+      margin: EdgeInsets.symmetric(horizontal: 30),
+      child: Text(
+        'Login',
+        style: TextStyle(
+            color: Colors.black,
+            fontWeight: FontWeight.bold,
+            fontSize: 28,
+            fontFamily: 'NimbusSans'),
+      ),
+    );
+  }
+
+  Widget _textFieldEmail() {
+    return Container(
+      margin: EdgeInsets.symmetric(horizontal: 30),
+      child: TextField(
+        decoration: InputDecoration(
+            hintText: 'correo@gmail.com',
+            labelText: 'Correo electronico',
+            suffixIcon: Icon(
+              Icons.email_outlined,
+              color: utils.Colors.uberCloneColor,
+            )),
+      ),
+    );
+  }
+
+  Widget _textFieldPassword() {
+    return Container(
+      margin: EdgeInsets.symmetric(horizontal: 30, vertical: 20),
+      child: TextField(
+        obscureText: true,
+        decoration: InputDecoration(
+            labelText: 'Password',
+            suffixIcon: Icon(
+              Icons.lock_open_outlined,
+              color: utils.Colors.uberCloneColor,
+            )),
+      ),
+    );
+  }
+
+  Widget _buttonLogin() {
+    return Container(
+      width: double.infinity,
+      margin: EdgeInsets.symmetric(horizontal: 30, vertical: 20),
+      child: RaisedButton(
+        onPressed: () {},
+        child: Text('Iniciar sesion', style: TextStyle(color: Colors.white)),
+        color: utils.Colors.uberCloneColor,
+      ),
+    );
+  }
+
+  Widget _dontHaveAccount() {
+    return Container(
+      margin: EdgeInsets.only(bottom: 50),
+      child: Text(
+        'No tines cuenta?',
+        style: TextStyle(fontSize: 15, color: Colors.grey),
       ),
     );
   }
