b00c1be2f727f8887dc78b3f0a60f9e36cde9489
Jhonatan-Cortez <jhonatancortez708@gmail.com>
2021-07-31 22:47:29 -0600

    Agregar elementos dentro del mapa

--- lib/src/pages/driver/map/driver_map_page.dart
+++ lib/src/pages/driver/map/driver_map_page.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter/scheduler.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:uberapp/src/pages/driver/map/driver_map_controller.dart';
+import 'package:uberapp/src/widgets/button_app.dart';
 
 class DriverMapPage extends StatefulWidget {
 
@@ -22,7 +23,27 @@ class _DriverMapPageState extends State<DriverMapPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: _googleMapsWidget()
+      // body: _googleMapsWidget()
+      body: Stack(
+        children: [
+          _googleMapsWidget(),
+          SafeArea(
+          child: Column(
+            children: [
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    _buttonDrawer(),
+                    _buttonCenterPosition()
+                  ]
+                ),
+                Expanded(child: Container()),
+                _buttonConect()
+              ],
+            ),
+          )
+        ],
+      )
     );
   }
 
@@ -31,6 +52,45 @@ class _DriverMapPageState extends State<DriverMapPage> {
       mapType: MapType.normal,
       initialCameraPosition: _con.initialPosition,
       onMapCreated: _con.onMapCreated,
+    );
+  }
+
+  Widget _buttonConect(){
+    return Container(
+      height: 50,
+      margin: EdgeInsets.symmetric(horizontal: 60, vertical: 30),
+      child: ButtonApp(
+        text: 'CONECTARSE', 
+        color: Colors.amber,
+        textColor: Colors.black,
+      ),
+    );
+  }
+
+  Widget _buttonDrawer(){
+    return Container(
+      alignment: Alignment.centerLeft,
+      child: IconButton(
+        onPressed: () {},
+        icon: Icon(Icons.menu, color: Colors.white,),
+      ),
+    );
+  }
+
+  Widget _buttonCenterPosition(){
+    return Container(
+      margin: EdgeInsets.symmetric(horizontal: 5),
+      child: Card(
+        shape: CircleBorder(),
+        child: Container(
+          padding: EdgeInsets.all(10),
+          child: Icon(
+            Icons.location_searching,
+            color: Colors.grey[600],
+            size: 20,
+          ),
+        ),
+      ),
     );
   }
 }
