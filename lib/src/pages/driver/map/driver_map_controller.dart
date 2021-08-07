356299c13122474f68b6b6145af7b2da1284311d
Jhonatan-Cortez <jhonatancortez708@gmail.com>
2021-08-07 14:53:38 -0600

    animar marcador con direccion

--- lib/src/pages/driver/map/driver_map_controller.dart
+++ lib/src/pages/driver/map/driver_map_controller.dart
@@ -141,7 +141,12 @@ class DriverMapController{
       markerId: id,
       icon: iconMarker,
       position: LatLng(lat, lng),
-      infoWindow: InfoWindow(title: title, snippet: content)
+      infoWindow: InfoWindow(title: title, snippet: content),
+      draggable: false,
+      zIndex: 2,
+      flat: true,
+      anchor: Offset(0.5, 0.5),
+      rotation: _position.heading
     );
 
     markers[id] = marker; 
